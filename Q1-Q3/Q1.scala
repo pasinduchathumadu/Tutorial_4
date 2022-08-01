521958343182bc8b7c02e1f1d0e551dcf25aba60
pasinduchathumadu <pasindugura123@gmail.com>
2022-08-01 21:04:51 +0530

    Update Q1.scala

--- Q1-Q3/Q1.scala
+++ Q1-Q3/Q1.scala
@@ -6,7 +6,7 @@ object Q1{
         println("The interest is:"+interest(amount))  //calling the function
     }
     def interest(amount:Double):Double=amount match{
-        case x if x<0 => 0;
+        case x if x<0 => 0
         case x if x<20000 =>0.02 * x
         case x if x<200000 =>0.04 * x
         case x if x<2000000 =>0.035 * x
